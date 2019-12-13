4c96864adda7b9383882158286851fe2c15de4bd
igorgo-hyuna <igor.gologots@hyuna.bb>
2019-12-13 18:40:00 +0200

    Added header

--- app/js/common.js
+++ app/js/common.js
@@ -8,62 +8,49 @@ $(document).ready(function() {
         $(".top_mnu").fadeOut(600);
         $(".sandwich").toggleClass("active");
         $(".top_text").css("opacity", "1");
-    }).append("<span>");
+    });
 
     /* Burger. Open and close */
     $(".toggle_mnu").click(function() {
         if ($(".top_mnu").is(":visible")) {
+            $('.top_mnu').css('left', '-581px');
             $(".top_text").css("opacity", "1");
             $(".top_mnu").fadeOut(600);
             $(".top_mnu li a").removeClass("fadeInUp animated");
+            $(".sw-topper, .sw-bottom, .sw-footer, .toggle_mnu a").css("opacity", "1");
         } else {
+            $('.top_mnu').css('left', '0');
             $(".top_text").css("opacity", ".1");
             $(".top_mnu").fadeIn(600);
             $(".top_mnu li a").addClass("fadeInUp animated");
+            $(".sw-topper, .sw-bottom, .sw-footer, .toggle_mnu a").css("opacity", "0");
         };
     });
 
-    /* Parallax 
-    $('.parallax-window').parallax({imageSrc: 'img/background.jpg'}); */
-
-    /* Resize
-    function heightDetect(){
-        $(".parallax-window").css("height", $(window).height());    
+    /********** Sticky menu **********/
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() { myFunction() };
+    // Get the navbar
+    var navbar = document.getElementById("navbar");
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+    // Проверка при перезагрузке странице в уже опущенном положении (дописано)
+    if (window.pageYOffset > sticky) {
+        navbar.classList.remove("no-sticky");
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+        navbar.classList.add("no-sticky");
     }
-    heightDetect();
-    $(window).resize(function(){
-        heightDetect();
-    });
-     */
-
-    /* Scrool animation
-    $(window).scroll(function() {
-
-        $('.mov').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeIn animated');
-                $(this).css('opacity', '1');
-            }
-        });
-
-        $('.mov-left').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeInLeft animated');
-                $(this).css('opacity', '1');
-            }
-        });
-
-        $('.mov-right').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeInRight animated');
-                $(this).css('opacity', '1');
-            }
-        });
-    });*/
+    // Проверка при перезагрузке странице в уже опущенном положении (дописано)
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            navbar.classList.remove("no-sticky");
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+            navbar.classList.add("no-sticky");
+        }
+    }
 });
